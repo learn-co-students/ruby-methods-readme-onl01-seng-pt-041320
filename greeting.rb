f381b7ba1b4a9028c81a40f77a38fcdf13f180a5
Jessica Gamez <jpgamez1789@icloud.com>
2020-04-15 22:51:48 +0000

    Done.

--- greeting.rb
+++ greeting.rb
@@ -1,3 +1,13 @@
-def greeting
-  puts "Hello World"
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
 end
