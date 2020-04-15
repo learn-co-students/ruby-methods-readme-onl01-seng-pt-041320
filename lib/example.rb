f0bdc10ff30bff3be4dd5c0b760b4ca9ef9b4ced
Antonio Espinoza <aespinoza777@gmail.com>
2020-04-15 03:18:19 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,4 @@
-# Use this space to code along with the readme
-
+def say_hello_ten_times
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -10,4 +9,4 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
-puts phrase
+end
